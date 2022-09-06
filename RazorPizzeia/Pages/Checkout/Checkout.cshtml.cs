330d9cc6484597860998f7f86bd4d6ddc47b585b
amir hossein <amirho3ein271@gmail.com>
2022-09-07 00:00:15 +0430

    update2

---
+++ RazorPizzeia/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeia.Pages.Checkout
+{
+    [BindProperties(SupportsGet = true)]
+
+    public class CheckoutModel : PageModel
+    {
+        public string PizzaName { get; set; }
+
+        public float PizzaPrice { get; set; }
+
+        public string ImageTitle { get; set; }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+        }
+    }
+}
---
+++ RazorPizzeia/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeia.Pages.Checkout
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
